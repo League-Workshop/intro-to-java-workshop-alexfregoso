b1741f1618d39c647219ef2895e1b7e886915a2c
alexfregoso <league@WorthyMini-5.attlocal.net>
2018-04-12 14:53:18 -0700

    i made a question thing

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -1,14 +1,16 @@
-package section4;
+00package section4;
 
 import java.util.Random;
 
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+	public static void main(String[] args) {
+		
+	 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
-	// 3. Print out this variable
+Random randgen = new Random  xm.t Int(100);
+	// 3. Print out this vailable
 
 	// 4. Get the user to enter something that they think is awesome
 
@@ -17,8 +19,8 @@ public class AwesomeOrNot {
 	// -- tell the user whatever they entered is awesome!
 
 	// 6. If the random number is 1
-
-	// -- tell the user whatever they entered is ok.
+i
+	// -- tell the user whatever they entered ok.
 
 	// 7. If the random number is 2
 
@@ -27,7 +29,7 @@ public class AwesomeOrNot {
 	// 8. If the random number is 3
 
 	// -- write your own answer
-
+	}
 }
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,36 @@
 package section4;
 
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score =0;
 		// 2.  Ask the user a question 
-		
+		String hi5490 = JOptionPane.showInputDialog(null, "whats 2+2");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(hi5490.equals("99002")){
 		// 4.  if the user's answer is correct
-		
+			JOptionPane.showMessageDialog(null,"YAY YOU GOT THE QUESTON RIGHT YOU ARE NOT STUPID THE FIRST GUY TO WIN YAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+			
 		// -- add one to their score 
+			score=score + 1;
+			
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+			String hi5499 = JOptionPane.showInputDialog(null, "whats 9999999999 + 9999999999999");
+			if(hi5499.equals("99001"))
+			 {
+				JOptionPane.showMessageDialog(null,"YAY YOU GOT THE QUESTON RIGHT YOU ARE NOT STUPID, THE FIRST GUY TO WIN YAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+			}
+		}
+			
 		// 6.  After all the questions have been asked, print the user's score 
 		
 	}
+
+	
 }
